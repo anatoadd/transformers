41d51c21edaa748e23d9bf1ae1dc26ca23deb93e
anatoadd <anato@outlook.jp>
2025-02-19 20:48:37 +0900

    update

--- .circleci/parse_test_outputs.py
+++ .circleci/parse_test_outputs.py
@@ -2,17 +2,23 @@ import re  # 正規表現を使うために re モジュールをインポート
 import argparse
 
 def parse_pytest_output(file_path):
+    # 辞書とカウンターの初期化
     skipped_tests = {}
     skipped_count = 0
+    # ログファイルの読み取り
     with open(file_path, 'r') as file:
         for line in file:
+            # 正規表現を使ってスキップされたテストの情報を抽出
             match = re.match(r'^SKIPPED \[(\d+)\] (tests/.*): (.*)$', line)
+            # スキップされたテストの集計
             if match:
                 skipped_count += 1
                 test_file, test_line, reason = match.groups()
                 skipped_tests[reason] = skipped_tests.get(reason, []) + [(test_file, test_line)]
+    # スキップ理由ごとの統計表示
     for k,v in sorted(skipped_tests.items(), key=lambda x:len(x[1])):
         print(f"{len(v):4} skipped because: {k}")
+    # スキップされた総数の表示
     print("Number of skipped tests:", skipped_count)
 
 def parse_pytest_failure_output(file_path):
